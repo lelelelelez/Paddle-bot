e10b3a10c2a3218afddf88201d1bce2996bb5e07
zhangchunle <clzhang_cauc@163.com>
2020-04-01 17:42:11 +0800

    fix

--- webservice/main.py
+++ webservice/main.py
@@ -10,13 +10,13 @@ routes = web.RouteTableDef()
 
 router = routing.Router()
 
-@router.register("issues", action="opened")
-async def issue_opened_event(event, gh, *args, **kwargs):
+@router.register("pull_request", action="opened")
+async def pull_request_event(event, gh, *args, **kwargs):
     """
     Whenever an issue is opened, greet the author and say thanks.
     """
-    url = event.data["issue"]["comments_url"]
-    author = event.data["issue"]["user"]["login"]
+    url = event.data["pull_request"]["url"]
+    author = event.data["pull_request"]["user"]["login"]
 
     message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
     await gh.post(url, data={"body": message})
