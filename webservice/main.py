80b12cc6d55ac2d64131bbd3297467210d357a09
zhangchunle <clzhang_cauc@163.com>
2020-04-02 14:35:24 +0800

    issue

--- webservice/main.py
+++ webservice/main.py
@@ -10,7 +10,7 @@ routes = web.RouteTableDef()
 
 router = routing.Router()
 
-@router.register("issues", action="created")
+@router.register("issues", action="opened")
 async def issues_event(event, gh, *args, **kwargs):
     """
     Whenever an issue is opened, greet the author and say thanks.
@@ -18,7 +18,7 @@ async def issues_event(event, gh, *args, **kwargs):
     url = event.data["issue"]["comments_url"]
     author = event.data["issue"]["user"]["login"]
 
-    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    message = f"Thanks for the report @{author}! opened issues! (I'm a bot)."
     await gh.post(url, data={"body": message})
 
 
