83b7658c47d7593c10debf6eb065e61730c2b6ce
zhangchunle <clzhang_cauc@163.com>
2020-04-02 17:26:24 +0800

    bot.py

--- webservice/bot.py
+++ webservice/bot.py
@@ -7,6 +7,65 @@ import jwt
 from gidgethub.aiohttp import GitHubAPI
 
 
+from aiohttp import web
+
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+routes = web.RouteTableDef()
+
+router = routing.Router()
+
+@router.register("issues", action="opened")
+async def issues_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! opened issues! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+@router.register("issue_comment", action="created")
+async def issues_comment_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! issue_comment issues! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+
+@router.register("pull_request", action="reopened")
+async def pull_request_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["pull_request"]["issue_url"]
+    author = event.data["pull_request"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! Reopened!! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+@router.register("pull_request", action="closed")
+async def pull_request_event_close(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["pull_request"]["issue_url"]
+    author = event.data["pull_request"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! closed!! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+
 def get_jwt(app_id):
 
     # TODO: read is as an environment variable
@@ -57,12 +116,21 @@ async def get_installation_access_token(gh, jwt, installation_id):
     return response
 
 
-async def main():
+
+
+@routes.post("/")
+async def main(request):
+    body = await request.read()
+
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
     async with aiohttp.ClientSession() as session:
         app_id = os.getenv("GH_APP_ID")
-
         jwt = get_jwt(app_id)
-        gh = GitHubAPI(session, "lelelelelez")
+        gh = gh_aiohttp.GitHubAPI(session, "lelelelelez")
 
         try:
             installation = await get_installation(gh, jwt, "lelelelelez")
@@ -78,6 +146,10 @@ async def main():
             # treat access_token as if a personal access token
 
             # Example, creating a GitHub issue as a GitHub App
+
+            gh = gh_aiohttp.GitHubAPI(session, "lelelelelez",
+                          oauth_token=access_token["token"])
+            '''
             gh_app = GitHubAPI(session, "black_out", oauth_token=access_token["token"])
             await gh_app.post(
                 "/repos/lelelelelez/leetcode/issues",
@@ -86,6 +158,16 @@ async def main():
                     "body": "Use more emoji! (I'm a GitHub App!) ",
                 },
             )
+            '''
+            await router.dispatch(event, gh)
+    return web.Response(status=200)
 
+#asyncio.run(main())
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes(routes)
+    port = os.environ.get("PORT")
+    if port is not None:
+        port = int(port)
 
-asyncio.run(main())
+    web.run_app(app, port=port)
