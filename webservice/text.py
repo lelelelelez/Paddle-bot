ea140e8529bfd4433143f7b3f8c66d13d7fe2ba6
zhangchunle <clzhang_cauc@163.com>
2020-04-14 15:35:06 +0800

    add test.py

---
+++ webservice/text.py
@@ -0,0 +1,6 @@
+pull_request_opened =u"""Thanks for your contribution! 
+    Please adding `test = develop` in your commit message to trigger CI.
+    See [Paddle CI Manual](https://github.com/PaddlePaddle/Paddle/wiki/paddle_ci_manual.md) for details.
+    """
+
+
