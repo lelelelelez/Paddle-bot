ebe152a754c01c4ec64f1bd57d541c6287b0be4b
lelelelelez <clzhang_cauc@163.com>
2020-04-21 15:20:14 +0800

    put

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -8,7 +8,7 @@ from gidgethub import aiohttp as gh_aiohttp
 import text
 from check import checkPRCI, checkPRTemplate
 from auth import get_jwt, get_installation, get_installation_access_token
-
+import datetime
 routes = web.RouteTableDef()
 router = routing.Router()
 
@@ -16,6 +16,10 @@ async def create_check_run(sha, gh):
     data = {'name': 'CheckPRTemplate', 'head_sha': sha}
     url = 'https://api.github.com/repos/lelelelelez/leetcode/check-runs'
     await gh.post(url, data=data, accept='application/vnd.github.antiope-preview+json')
+
+async def init_check_run(url, checkrunName, gh):
+    data = {"name": checkrunName, "status": "in_progress"}    
+    await gh.patch(url, data=data, accept='application/vnd.github.antiope-preview+json')
 
 @router.register("pull_request", action="opened")
 @router.register("pull_request", action="synchronize")
@@ -37,9 +41,10 @@ async def pull_request_event_template(event, gh, *args, **kwargs):
     url = event.data["pull_request"]["comments_url"]  
     BODY = event.data["pull_request"]["body"]
     sha = event.data["pull_request"]["head"]["sha"]
-    create_check_run(sha, gh)
+    await create_check_run(sha, gh)
     global check_pr_template
     check_pr_template = checkPRTemplate(BODY)
+    print(check_pr_template)
     if check_pr_template == False:
         message = text.NOT_USING_TEMPLATE
         await gh.post(url, data={"body": message})
@@ -47,17 +52,44 @@ async def pull_request_event_template(event, gh, *args, **kwargs):
 @router.register("check_run", action="created")
 async def process_check_run(event, gh, *args, **kwargs):
     url = event.data["check_run"]["url"]
+    print(url)
     name = event.data["check_run"]["name"]
-    started_at = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+    data = {"name": name, "status": "in_progress"}
+    print(await gh.patch(url, data=data, accept='application/vnd.github.antiope-preview+json'))
     time.sleep(5)
+    print("check_pr_template:%s" %check_pr_template)
     if check_pr_template == False:
-        data = {"name": name, "started_at": started_at, "status": "completed", 'conclusion': 'failure'}
+        data = {"name": name, "status": "completed", 'conclusion': 'failure'}
     else:
-        data = {"name": name, "started_at": started_at, "status": "completed", 'conclusion': 'success'}
+        data = {"name": name, "status": "completed", 'conclusion': 'success'}
     response = await gh.patch(url, data=data, accept='application/vnd.github.antiope-preview+json')
     print(response)
-
-#@routes.post("/")
+'''
+@routes.patch("/")
+async def patch_main(request):
+    body = await request.read()
+    secret = os.environ.get("GH_SECRET")
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+    async with aiohttp.ClientSession() as session:
+        app_id = os.getenv("GH_APP_ID")
+        jwt = get_jwt(app_id)
+        gh = gh_aiohttp.GitHubAPI(session, "lelelelelez")
+        try:
+            installation = await get_installation(gh, jwt, "lelelelelez")
+        except ValueError as ve:
+            print(ve)
+        else:
+            access_token = await get_installation_access_token(
+                gh, jwt=jwt, installation_id=installation["id"]
+            )
+            # treat access_token as if a personal access token
+            gh = gh_aiohttp.GitHubAPI(session, "lelelelelez",
+                        oauth_token=access_token["token"])
+            await process_check_run(event, gh)
+	    #await router.dispatch(event, gh)
+    return web.Response(status=200)
+'''
+@routes.post("/")
 async def main(request):
     body = await request.read()
     secret = os.environ.get("GH_SECRET")
@@ -80,6 +112,9 @@ async def main(request):
                         oauth_token=access_token["token"])
             await router.dispatch(event, gh)
     return web.Response(status=200)
+@routes.get("/")
+async def main(request):
+    return web.Response(status=200, text="Hello world!")
 
 if __name__ == "__main__":
     app = web.Application()
--- webservice/text.py
+++ webservice/text.py
@@ -1,6 +1,12 @@
-PULL_REQUEST_OPENED =u"""Thanks for your contribution! 
-    Please adding `test = develop` in your commit message to trigger CI.
+PULL_REQUEST_OPENED_NOT_CI =u"""Thanks for your contribution! 
+    Please adding `test = develop` in your commit message to trigger CI to ensure your's PR can be merged.
     See [Paddle CI Manual](https://github.com/PaddlePaddle/Paddle/wiki/paddle_ci_manual.md) for details.
     """
-USING_TEMPLATE = u""""""
-
+PULL_REQUEST_OPENED = u"""Thanks for your contribution! 
+    Please wait for the result of CI firstly. See [Paddle CI Manual](https://github.com/PaddlePaddle/Paddle/wiki/paddle_ci_manual.md) for details.
+    """
+NOT_USING_TEMPLATE =u"""This PR is not created using [PR template](https://github.com/lelelelelez/leetcode/blob/master/.github/PULL_REQUEST_TEMPLATE) so I'm going to close it.
+    Sorry for this, but it helps save our maintainers' time so that more developers get helped.
+    Feel free to create another pr using the issue template.
+    If you think you have already made your point clear without the template, or your problem cannot be covered by it, you may re-open this issue again.
+    """
