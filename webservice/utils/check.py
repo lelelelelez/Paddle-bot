afe46deb5fd222dcb126d9117e46a66802ca0e2c
zhangchunle <clzhang_cauc@163.com>
2020-05-14 16:39:21 +0800

    modify pr template check

--- webservice/utils/check.py
+++ webservice/utils/check.py
@@ -34,20 +34,25 @@ def checkPRTemplate(body, CHECK_TEMPLATE, CHECK_TEMPLATE_doc=None):
         res: True or False
     """
     res = False
+    note = '\*\*|<!-- ADD SCREENSHOT HERE IF APPLICABLE. -->|<!-- DESCRIBE THE BUG OR REQUIREMENT HERE. eg. #2020（格式为 #Issue编号）-->|-----------------------'
+    body = re.sub(note, "", body)
     if CHECK_TEMPLATE_doc != None:
-        print(CHECK_TEMPLATE_doc)
-        note1 = '<!-- ADD SCREENSHOT HERE IF APPLICABLE. -->'
-        note2 = '<!-- DESCRIBE THE BUG OR REQUIREMENT HERE. eg. #2020（格式为 #Issue编号）-->'
-        body_no_note = re.sub(note2, "", re.sub(note1, "", body))
-        doc_check = "- PR changes:（改动点）is \(\s*[A-D]*[C][A-D]*\s*\):"
+        doc_check = "- PR changes（改动点）is \(\s*[A-D]*[C][A-D]*\s*\):"
         match_doc = re.search(doc_check, body, re.M|re.I)
-        print(match_doc)
-        if match_doc != None:
-            result_doc = re_rule(body_no_note, CHECK_TEMPLATE_doc)
+        if match_doc != None: #choose doc changes
+            others ="- Please write down other information you want to tell reviewers.(.*[^\s].*)"
+            if re_rule(body, others) == None:
+                body = re.sub('- Please write down other information you want to tell reviewers.', "", body)
+                CHECK_TEMPLATE_doc = "#### Required（必填, multiple choices, two at most）\r\n- PR type（PR 类型） is \(\s*[A-F]+\s*\):(.*?)- PR changes（改动点）is \(\s*[A-D]*[C][A-D]*\s*\):(.*?)- Use one sentence to describe what this PR does.（简述本次PR的目的和改动）(.*[^\s].*)#### Optional（选填, If None, please delete it）(.*?)- If you modified docs, please make sure that both Chinese and English docs were modified and provide a preview screenshot. （文档必填）(.*[^\s].*)"
+            result_doc = re_rule(body, CHECK_TEMPLATE_doc)
             if result_doc != None:
                 res = True
             return res
 
+    option_check = "#### Optional（选填, If None, please delete it）"
+    match_option = re.search(option_check, body, re.M|re.I)
+    if match_option == None:
+        CHECK_TEMPLATE = "﻿#### Required（必填, multiple choices, two at most）\r\n- PR type（PR 类型） is \(\s*[A-F]+\s*\):(.*?)- PR changes（改动点）is \(\s*[A-D]+\s*\):(.*?)- Use one sentence to describe what this PR does.（简述本次PR的目的和改动）(.*[^\s].*)"
     result = re_rule(body, CHECK_TEMPLATE)
     if len(CHECK_TEMPLATE) == 0 and len(body) == 0:
         res = False
